1fcdeeb09535379a3bd90399d226bdcb2c217ce1
Saad Bakht <rotk13@gmail.com>
2017-10-08 13:39:45 -0400

    ignores input and shows starting value

--- rogue.hs
+++ rogue.hs
@@ -2,6 +2,13 @@ module Main where
 
 main :: IO ()
 main = do
+  loop
+
+loop :: IO ()
+loop = do
   input <- getLine
-  putStrLn $ "Rogue code here - " ++  input
-  main
+  putStrLn $ "Rogue code here - " ++ start
+  loop
+
+start :: String
+start = "0"
