6b29b3ada929781b998b3b4154aacacbe83ec662
Saad Bakht <saad.bakht@bloomingdales.com>
2017-10-09 14:46:13 -0400

    show game board with controllable character

--- rogue.hs
+++ rogue.hs
@@ -1,7 +1,7 @@
 module Main where
 
 
-data Cord = Cord (Int, Int)
+data Cord = Cord (Int, Int) deriving (Show)
 
 data Input =
     IUp
@@ -10,32 +10,62 @@ data Input =
   | IRight 
     deriving (Show)
 
+data Player = Player { cord :: Cord } deriving (Show)
+
 isValid :: String -> Bool
 isValid = undefined
 
-getInput :: IO (String)
-getInput = getLine
+getInput :: IO (Maybe Input)
+getInput = do 
+    input <- getLine
+    return $ toInput input
 
-runInput :: [Input] -> String -> [Input]
-runInput inputs s =
-    case toInput s of
-        (Just x) -> x : inputs
-        Nothing  -> inputs
+runInput :: [Input] -> Maybe Input -> [Input]
+runInput inputs (Just x) = x : inputs
+runInput inputs Nothing = inputs
 
 toInput :: String -> Maybe Input
-toInput "1" = Just ILeft
-toInput "2" = Just IUp
-toInput "3" = Just IRight
-toInput "4" = Just IDown
+toInput "a" = Just ILeft
+toInput "w" = Just IUp
+toInput "d" = Just IRight
+toInput "s" = Just IDown
 toInput _ = Nothing
+
+movePlayer :: Player -> Maybe Input -> Player
+movePlayer (Player cord) (Just input) = Player (moveObject cord input)
+movePlayer player Nothing = player
+
+moveObject :: Cord -> Input -> Cord
+moveObject (Cord (x,y)) IUp = Cord (x, y - 1)
+moveObject (Cord (x,y)) IDown = Cord (x, y + 1)
+moveObject (Cord (x,y)) IRight = Cord (x + 1, y)
+moveObject (Cord (x,y)) ILeft = Cord (x - 1, y)
+
+playerStart :: Player
+playerStart = Player (Cord (0,0))
+
+showWorld :: Player -> IO ()
+showWorld player = putStrLn $ buildWorld (cord player) 3 3 
+
+buildWorld :: Cord -> Int -> Int -> String
+buildWorld (Cord (c,c')) n n' = go 0 0 
+    where go x y 
+            | (y == n') = [] 
+            | (x == n)  = ['\n'] ++ go 0 (y + 1)
+            | (x == c && y == c') = ['@'] ++ go (x + 1) y
+            | otherwise = ['-'] ++ go (x + 1) y
+
 
 main :: IO ()
 main = do
-  loop []
+  loop [] (Player (Cord (0,0)))
 
-loop :: [Input] -> IO ()
-loop prevInputs = do
+loop :: [Input] -> Player -> IO ()
+loop prevInputs player = do
   input <- getInput
   let inputs = runInput prevInputs input
+  let player' = movePlayer player input
   putStrLn $ "Rogue code here - " ++ show inputs
-  loop inputs
+  showWorld player'
+  print player'
+  loop inputs player'
