084f67d90130850d3d14f951bec683debee0a3cd
Saad Bakht <saad.bakht@bloomingdales.com>
2017-10-09 12:02:14 -0400

    take 4 directional inputs

--- rogue.hs
+++ rogue.hs
@@ -1,12 +1,41 @@
 module Main where
 
+
+data Cord = Cord (Int, Int)
+
+data Input =
+    IUp
+  | IDown
+  | ILeft
+  | IRight 
+    deriving (Show)
+
+isValid :: String -> Bool
+isValid = undefined
+
+getInput :: IO (String)
+getInput = getLine
+
+runInput :: [Input] -> String -> [Input]
+runInput inputs s =
+    case toInput s of
+        (Just x) -> x : inputs
+        Nothing  -> inputs
+
+toInput :: String -> Maybe Input
+toInput "1" = Just ILeft
+toInput "2" = Just IUp
+toInput "3" = Just IRight
+toInput "4" = Just IDown
+toInput _ = Nothing
+
 main :: IO ()
 main = do
   loop []
 
-loop :: [String] -> IO ()
-loop start = do
-  input <- getLine
-  let accum = start ++ [input]
-  putStrLn $ "Rogue code here - " ++ show accum
-  loop accum
+loop :: [Input] -> IO ()
+loop prevInputs = do
+  input <- getInput
+  let inputs = runInput prevInputs input
+  putStrLn $ "Rogue code here - " ++ show inputs
+  loop inputs
